9d443136109bccb0c53c0353ba176e10a0bab331
keyurbm <kaysmakwana@gmail.com>
2020-08-23 20:07:51 +0530

    Driver manager Example

---
+++ src/test/java/com/test/SeleniumTest/ChromeTest.java
@@ -0,0 +1,39 @@
+package com.test.SeleniumTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ChromeTest {
+	 private WebDriver driver;
+
+	    @BeforeSuite
+	    public static void setupClass() {
+	        WebDriverManager.chromedriver().setup();
+	    }
+
+	    @BeforeTest
+	    public void setupTest() {
+	        driver = new ChromeDriver();
+	    }
+
+	    @AfterTest
+	    public void teardown() {
+	        if (driver != null) {
+	            driver.quit();
+	        }
+	    }
+
+	    @Test
+	    public void test() {
+	        driver.get("https://google.com");
+	    }
+}
